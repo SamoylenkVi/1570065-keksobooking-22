903935fa413eea9ded6a850103482c6b4e69da30
SamoylenkVi <SamoylenkVi@users.noreply.github.com>
2021-01-31 21:42:00 +0300

    Добавляет функцию для получения числа с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 const getRandomInteger = function(min, max) {
   if (max < min) {
-    const savedMax =max;
+    const savedMax = max;
     max = min;
     min= savedMax;
   }
 
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
+
+const getRandomNumber = function (min, max, fixed) {
+  if (max < min) {
+    const savedMax = max;
+    max = min;
+    min= savedMax;
+  }
+
+  const random = min + Math.random() * (max - min);
+  const multiplier = Math.pow(10, fixed);
+  return Math.round(random * multiplier) / multiplier;
+}
+
+getRandomInteger(5, 10);
+getRandomNumber(0.1, 0.2, 5);
