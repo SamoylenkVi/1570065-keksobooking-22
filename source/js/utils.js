27e153a64fd71159c95a0cadf77654bd211b0ec4
SamoylenkVi <SamoylenkVi@users.noreply.github.com>
2021-03-23 19:33:38 +0300

    добавляет строгое сравннение в utils

--- source/js/utils.js
+++ source/js/utils.js
@@ -1,7 +1,7 @@
 const DEBOUNCE_INTERVAL = 500;
 
 const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
-  if (num > 10 && (Math.round((num % 100) / 10)) == 1) {
+  if (num > 10 && (Math.round((num % 100) / 10)) === 1) {
     return genitivePlural;
   } else {
     switch (num % 10) {
